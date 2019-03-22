08599ee103a3a9b9780108ed16536461f50b149f
Ravensscry <whitequincy1907@gmail.com>
2019-03-23 02:22:56 +0300

    xd

---
+++ BMO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class BMO
+    {
+
+
+
+        private LinkedList<string> skills
+        {
+            get { return skills; }
+            set { this.skills = value; }
+        }
+        private string city
+        {
+            get { return city; }
+            set { this.city = value; }
+        }
+        private string email
+        {
+            get { return email; }
+            set { this.email = value; }
+        }
+        private DateTime dateofbirth
+        {
+            get { return dateofbirth; }
+            set { this.dateofbirth = value; }
+        }
+        private string phonenumber
+        {
+            get { return phonenumber; }
+            set { this.phonenumber = value; }
+        }
+
+    }
+}
